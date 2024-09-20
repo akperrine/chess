6465964d4e0833dbd1f7fb0c6d0e6488470cb04d
Austin Perrine <akperrine@gmail.com>
2024-09-19 20:44:59 -0700

    add knight moves

--- src/knight.cpp
+++ src/knight.cpp
@@ -23,6 +23,38 @@ namespace chess_game {
 
     std::vector<std::pair<int, int>> Knight::get_moves(Square board[8][8], int x, int y)  {
         std::vector<std::pair<int, int>> moves = std::vector<std::pair<int, int>>();
+        if (y + 2 <= 7) {
+            if (x + 1 <= 7 && !(board[x+1][y+2].piece && board[x+1][y+2].piece->is_light == is_light)) {
+                moves.push_back(std::make_pair(x + 1, y + 2));
+            }
+            if (x - 1 >= 0 && !(board[x-1][y+2].piece && board[x-1][y+2].piece->is_light == is_light)) {
+                moves.push_back(std::make_pair(x - 1, y + 2));
+            }
+        }
+        if (y - 2 >= 0) {
+            if (x + 1 <= 7 && !(board[x+1][y-2].piece && board[x+1][y-2].piece->is_light == is_light)) {
+                moves.push_back(std::make_pair(x + 1, y - 2));
+            }
+            if (x - 1 >= 0 && !(board[x-1][y-2].piece && board[x-1][y-2].piece->is_light == is_light)) {
+                moves.push_back(std::make_pair(x - 1, y - 2));
+            }
+        }
+        if (x + 2 <= 7) {
+            if (y + 1 <= 7 && !(board[x+2][y+1].piece && board[x+2][y+1].piece->is_light == is_light)) {
+                moves.push_back(std::make_pair(x + 2, y + 1));
+            }
+            if (y - 1 >= 0 && !(board[x+2][y-1].piece && board[x+2][y-1].piece->is_light == is_light)) {
+                moves.push_back(std::make_pair(x + 2, y - 1));
+            }
+        }
+        if (x - 2 >= 0) {
+            if (y + 1 <= 7 && !(board[x-2][y+1].piece && board[x-2][y+1].piece->is_light == is_light)) {
+                moves.push_back(std::make_pair(x - 2 , y + 1));
+            }
+            if (y - 1 >= 0 && !(board[x-2][y-1].piece && board[x-2][y-1].piece->is_light == is_light)) {
+                moves.push_back(std::make_pair(x - 2, y - 1));
+            }
+        }
         
         return moves;
     }
